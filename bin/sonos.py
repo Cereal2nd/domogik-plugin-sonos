87cd00b19b7f8d1260813efe1157e90f224e0c9c
Maikel Punie <maikel.punie@gmail.com>
2016-10-19 15:34:21 +0200

    Added more support, switched to non-xpl plugin

--- bin/sonos.py
+++ bin/sonos.py
@@ -19,69 +19,78 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Domogik. If not, see U{http://www.gnu.org/licenses}.
 
-Plugin purpose
-==============
-
-Viera Device control
-
 @author: Punie Maikel <maikel.punie@gmail.com>
 @copyright: (C) 2007-2016 Domogik project
 @license: GPL(v3)
 @organization: Domogik
 """
 
-from domogik.xpl.common.xplconnector import Listener
-from domogik.xpl.common.xplmessage import XplMessage
-from domogik.xpl.common.plugin import XplPlugin
-from domogik_packages.plugin_viera.lib.viera import Viera
-from soco import SoCo
+from domogik.common.plugin import Plugin
+from soco import SoCo, discover
 import traceback
 
-class Bluez(XplPlugin):
+class Sonos(Plugin):
     '''
     Manage
     '''
     def __init__(self):
-        """
-        Create the bluez plugin.
-        """
-        XplPlugin.__init__(self, name = 'sonos')
+        Plugin.__init__(self, name='sonos')
         if not self.check_configured():
             self.force_leave()
             return
-        devices = self.get_device_list(quit_if_no_device = True)
-        Listener(self.send_command, self.myxpl,
-            {'xpltype': 'xpl-cmnd', 'schema': 'control.basic'})
-
+        self.devices = self.get_device_list(quit_if_no_device = True)
+        self._soco_to_dev = {}
+        self._map_device_to_soco()
+    
         # notify ready
         self.ready()
 
-    def send_command(self, msgr):
-        device = msgr.data['device']
-        typ = msgr.data['type']
-        self.log.info("Sending {0} to {1}".format(typ, device))
-        dev = SoCo(device)
-        if typ == "mute":
-            dev.mute()
-        elif typ == "play":
-            dev.play()
-        elif typ == "stop":
-            dev.stop()
-        elif typ == "volup":
-            vol = dev.volume
-            vol += 5
-            dev.volume = vol
-        elif typ == "voldown":
-            vol = dev.volume
-            vol -= 5
-            dev.volume = vol
-        # send stats message
-        msg = XplMessage()
-        msg.set_type("xpl-trig")
-        msg.set_schema("sensor.basic")
-        msg.add_data({'device' : device})
-        msg.add_data({'current' : typ})
-        self.myxpl.send(msg)
+    def _map_device_to_soco(self):
+        found = discover()
+        for dev in self.devices:
+            for fnd in found.copy():
+                if 'device' in dev['parameters'] and \
+                    fnd.player_name == dev['parameters']['device']['value']:
+                    self._soco_to_dev[dev['id']] = fnd
+                    found.remove(fnd)
+                else:
+                    print "Device does not have an soco device"
+        if len(found) != 0:
+            print "Not all sonos devices mapped"
+            print "Trigger a device_detected message"
+
+    def on_mdp_request(self, msg):
+        Plugin.on_mdp_request(self, msg)
+        if msg.get_action() == "client.cmd":
+            data = msg.get_data()
+	    if 'cmd' in data and 'dev' in data:
+		typ = data['cmd']
+		dev = self._soco_to_dev[data['dev']]
+		if typ == "mute":
+		    dev.mute()
+		elif typ == "play":
+		    dev.play()
+		elif typ == "pause":
+		    dev.pause)
+		elif typ == "stop":
+		    dev.stop()
+		elif typ == "next":
+		    dev.next()
+		elif typ == "previous":
+		    dev.previous()
+		elif typ == "volup":
+		    vol = dev.volume
+		    vol += 5
+		    dev.volume = vol
+		elif typ == "voldown":
+		    vol = dev.volume
+		    vol -= 5
+		    dev.volume = vol
+            reply_msg = MQMessage()
+            reply_msg.set_action('client.cmd.result')
+            reply_msg.add_data('status', True)
+            reply_msg.add_data('reason', None)
+            self.reply(reply_msg.get())
 
 if __name__ == "__main__":
-    Bluez()
+    Sonos()
